344076f3cac18f0139931530c24b62053be68e93
jose@exalt.cd <jose@exalt.cd>
2023-06-19 10:49:44 +0100

    products and catalog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TabsPipe } from './pages/tabs.pipe';
+import { ProductsPipe } from './pages/tabs/products.pipe';
+import { CartPipe } from './pages/tabs/cart.pipe';
+import { ServicesPipe } from './pages/tabs/services.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TabsPipe, ProductsPipe, CartPipe, ServicesPipe],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
